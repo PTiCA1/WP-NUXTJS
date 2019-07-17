291a23ab70db32fe6f9860dc9fbbe54a7fa3fff8
Libor Ptacek <libor.ptacek@wdf.cz>
2019-07-17 17:11:38 +0200

    update category pageId posts

--- store/posts/category.js
+++ store/posts/category.js
@@ -9,10 +9,14 @@ export const actions = {
     const slug = payload.slug
     const categoryId = payload.categoryId
     const pageId = payload.pageId
-    // const postsInPageIdExist = state.posts.some( item => item['pageId'] === pageId )
 
-    if ( !state.name.length ) {
-      const response = await this.$axios.$get(
+    // Kontroluju jestli uz kategorie neexistuje ve state.name
+    const thereIsCategory = Object.keys( state.name ).some( item => item === slug )
+
+    if ( !thereIsCategory ) {
+      // Category not exist in state.name
+
+      const response = await this.$axios.get(
         `posts?categories=${categoryId}&_embed&page=${pageId}`
       );
 
@@ -20,22 +24,80 @@ export const actions = {
       postPage[slug] = {
         name: slug,
         categoryId: categoryId,
-        pageId: pageId,
-        posts: response
+        totalPages: response.headers['x-wp-totalpages'],
+        posts: [
+          {
+            pageId: pageId,
+            posts: response.data
+          }
+        ]
       }
-      commit("addTest", postPage);
+      commit("add", postPage);
+
+    } else {
+      // Category exist in state.name
+      // Update articles in pageId
+      // Kategorie existuje ale nactu clanky na strance XY
+
+      // I check if there are articles in the category and pageId
+      // Kontrola jestli clanky pro stranky pageId existuji. Pokud ne, tak je nactu
+      const postsInCategoryAndPageIdExist = state.name[slug].posts.some( item => item['pageId'] === pageId )
+      if ( !postsInCategoryAndPageIdExist ) {
+
+        const response = await this.$axios.get(
+          `posts?categories=${categoryId}&_embed&page=${pageId}`
+        );
+
+        const postPageInCategory = {
+          pageId: pageId,
+          posts: response
+        }
+
+        commit("addPostsPageId", {name: slug, items: postPageInCategory });
+
+        console.log( 'snazim se pridat clanky pod strankou ID : ' + postsInCategoryAndPageIdExist );
+      }
+      console.log( 'clanky pro tuto kategorii: ' + postsInCategoryAndPageIdExist );
 
     }
+
+
+
+
+
+
+
+
+      // const response = await this.$axios.get(
+      //   `posts?categories=${categoryId}&_embed&page=${pageId}`
+      // );
+
+      // const postPage = {};
+      // postPage[slug] = {
+      //   name: slug,
+      //   categoryId: categoryId,
+      //   totalPages: response.headers['x-wp-totalpages'],
+      //   posts: [
+      //     {
+      //       pageId: pageId,
+      //       posts: response.data
+      //     }
+      //   ]
+      // }
+      // commit("add", postPage);
+
+
   }
 }
 
 export const mutations = {
-  add(state, articles) {
-    state.posts = [...state.posts, articles];
-  },
-  addTest(state, items) {
+  add(state, items) {
     state.name = Object.assign(state.name, items)
-  }
+  },
+  addPostsPageId(state, payload) {
+    state.name = Object.assign(state.name[payload.name].posts, payload.items)
+    // state.name = {...state.name[slug].posts, items};
+  },
 }
 
 export const getters = {
