a5b456083e51ed15ecd28bfb7ee77a5eff990694
Libor Ptacek <libor.ptacek@wdf.cz>
2019-07-30 17:16:56 +0200

    search form update

--- components/TheSearch.vue
+++ components/TheSearch.vue
@@ -11,8 +11,8 @@
       </div>
 
       <form class="search__form">
-        <label for="search-input" class="sr-only">Search</label>
-        <input type="text" id="search-input" placeholder="Search">
+        <label for="search-input">Search</label>
+        <input type="text" id="search-input" placeholder="Search" @keyup="searchValue">
       </form>
 
       <div class="search__result">
@@ -29,6 +29,7 @@
 
 <script>
 import { mapMutations, mapGetters } from 'vuex'
+import _ from 'lodash'
 
 import Close from "~/components/AppIconClose"
 
@@ -44,7 +45,10 @@ export default {
   methods: {
     ...mapMutations({
       toggle: 'search/toggle'
-    })
+    }),
+    searchValue: _.debounce(function (event) {
+      console.log(event.target.value);
+    }, 300)
   }
 }
 </script>
@@ -81,10 +85,37 @@ export default {
       margin-left: auto;
     }
   }
+
   &__form {
     flex: 0;
     padding: 2rem 1rem;
+
+    > label {
+      // Bootstrap .sr-only
+      // @link https://github.com/twbs/bootstrap/blob/a4a04cd9ec741050390746f8056cc79a9c04c8df/scss/mixins/_screen-reader.scss
+      position: absolute !important;
+      width: 1px !important;
+      height: 1px !important;
+      padding: 0 !important;
+      margin: -1px !important; // Fix for https://github.com/twbs/bootstrap/issues/25686
+      overflow: hidden !important;
+      clip: rect(0, 0, 0, 0) !important;
+      white-space: nowrap !important;
+      border: 0 !important;
+    }
+
+    > input {
+      display: block;
+      padding: 1rem;
+      color: var(--dark);
+      font-size: 2rem;
+
+      border-style: solid;
+      border-color: transparent transparent var(--gray) transparent;
+      border-width: 0 0 2px 0;
+    }
   }
+
   &__result {
     flex: 1;
   }
